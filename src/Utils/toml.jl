301c5903365d07e10d9cd1b2bf2c8f262a5ba27c
josePereiro <jose.pereiro.lab@gmail.com>
2020-07-02 15:36:08 -0400

    Add key toml comments support, Fix bug

--- src/Utils/toml.jl
+++ src/Utils/toml.jl
@@ -1,20 +1,43 @@
-function write_toml(file, toml_dict::Dict; 
-        headlines = [])
-    
+# Force single toml files
+TOML_DICT_TYPE = Dict{String, Union{String, Number}}
+
+function toml_commtstr(lines)
     tempio = IOBuffer();
-    
-    # head lines
-    foreach(headlines) do line
+    foreach(lines) do line
         line = isempty(line) ? "" : "# " * line
         println(tempio, line)
     end
-    TOML.print(tempio, toml_dict)
+    return String(take!(tempio))
+end
+
+function write_toml(file, dict; 
+        headcmmts = [],
+        keycmmts = Dict(),
+    )
+    
+    dict = TOML_DICT_TYPE(dict)
+    tempio = IOBuffer();
+    
+    # head lines
+    headstr = toml_commtstr(headcmmts)
+    println(tempio, headstr)
+
+    # key
+    for kv in dict
+        (k, val) = kv
+        cmmtstr = get(keycmmts, k, []) |> toml_commtstr
+        !isempty(cmmtstr) && print(tempio, cmmtstr)
+        TOML.print(tempio, kv |> Dict)
+        TOML.println(tempio)
+    end
+    
     toml_str = String(take!(tempio))
     close(tempio)
     
     open(file, "w") do io
         println(io, toml_str)
     end
+
 end
 
-read_toml(file) = TOML.parsefile("test.toml")
+read_toml(file) = TOML_DICT_TYPE(TOML.parsefile("test.toml"))
