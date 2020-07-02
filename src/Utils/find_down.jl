197ecf8424bbb2da903a17f8725bf417cced6af6
josePereiro <jose.pereiro.lab@gmail.com>
2020-07-02 16:17:18 -0400

    Add oerr to Utils finds

--- src/Utils/find_down.jl
+++ src/Utils/find_down.jl
@@ -1,34 +1,51 @@
 """
     Look down in the dirtree for files or dirs that make
-    'fun' returns true. 'fun' will receive the abspath 
-    of each dir or file. All matches will be stored in a 
+    'fun' returns true. All matches will be stored in a 
     container. Use 'retfun' to control when to return 
-    the container. 
+    the container.Also use 'onerr' to manage possibles 
+    errors.
     Returns all the matches abspath of an empty array
+    Signature:
+        fun(path)::Bool
+        retfun(path, container)::Bool
+        onerr(path, container, err)::Bool
 """
 function findall_down(fun::Function, rootpath; container = [], 
-        retfun = (container) -> false)
+        retfun = (path, container) -> false,
+        onerr = (path, container, err) -> nothing)
     rootpath = isdir(rootpath) ? rootpath : rootpath |> dirname
 
-    fun(rootpath) && push!(container, rootpath)
+    try
+        fun(rootpath) && push!(container, rootpath)
+        retfun(rootpath, container) && return container
+    catch err
+        onerr(rootpath, container, err) && return container
+    end
 
     for (root, dirs, files) in walkdir(rootpath)
         root = abspath(root)
 
-        fun(root) && push!(container, root)
+        try
+            fun(root) && push!(container, root)
+            retfun(root, container) && return container
+        catch err
+            onerr(rootpath, container, err) && return container
+        end
 
         for file in files
             file = joinpath(root, file)
-            fun(file) && push!(container, file)
-            retfun(container) && return container
+            try
+                fun(file) && push!(container, file)
+                retfun(file, container) && return container
+            catch err
+                onerr(rootpath, container, err) && return container
+            end
         end
     end
     return container
 end
-findall_down(name::AbstractString, rootpath; container = [],
-        retfun = (container) -> false) = 
-        findall_down((path) -> basename(path) == name, rootpath; 
-        container = container, retfun = retfun);
+findall_down(name::AbstractString, rootpath; kwargs...) = 
+        findall_down((path) -> basename(path) == name, rootpath; kwargs...);
 
 """
     Look down in the dirtree for the first files or dirs 
@@ -37,7 +54,7 @@ findall_down(name::AbstractString, rootpath; container = [],
     Returns an abspath or nothing 
 """
 function find_down(fun::Function, rootpath)
-    founds = findall_down(fun, rootpath; retfun = (container) -> length(container) == 1)
+    founds = findall_down(fun, rootpath; retfun = (path, container) -> length(container) == 1)
     return isempty(founds) ? nothing : founds |> first
 end
 find_down(name::AbstractString, rootpath) = find_down((path) -> basename(path) == name, rootpath);
--- src/Utils/find_up.jl
+++ src/Utils/find_up.jl
@@ -1,13 +1,18 @@
 """
     Look up in the dirtree for files or dirs that make
-    'fun' returns true. 'fun' will receive the abspath 
-    of each dir or file. All matches will be stored in a 
+    'fun' returns true. All matches will be stored in a 
     container. Use 'retfun' to control when to return 
-    the container. 
+    the container. Also use 'onerr' to manage possibles 
+    errors.
     Returns all the matches abspath of an empty array
+    Signature:
+        fun(path)::Bool
+        retfun(path, container)::Bool
+        onerr(path, container, err)::Bool
 """
 function findall_up(fun::Function, rootpath; container = [], 
-        retfun = (container) -> false)
+        retfun = (path, container) -> false,
+        onerr = (path, container, err) -> nothing)
     
     rootpath = rootpath |> abspath
     rootpath = isdir(rootpath) ? rootpath : rootpath |> dirname
@@ -18,8 +23,12 @@ function findall_up(fun::Function, rootpath; container = [],
     # find in root
     for bname in readdir(rootpath)
         path = joinpath(rootpath, bname)
-        fun(path) && push!(container, path)
-        retfun(container) && return container
+        try
+            fun(path) && push!(container, path)
+            retfun(path, container) && return container
+        catch err
+            onerr(path, container, err) && return container
+        end
     end
 
     # Base
@@ -31,10 +40,8 @@ function findall_up(fun::Function, rootpath; container = [],
 end
 
 
-findall_up(name::AbstractString, rootpath; container = [], 
-        retfun = (container) -> false) =
-    findall_up((path) -> basename(path) == name, rootpath; 
-        container = container, retfun  = retfun)
+findall_up(name::AbstractString, rootpath; kwargs...) =
+    findall_up((path) -> basename(path) == name, rootpath; kwargs...)
 
         
 """
@@ -44,8 +51,9 @@ findall_up(name::AbstractString, rootpath; container = [],
     Returns an abspath or nothing 
 """
 function find_up(fun::Function, rootpath)
-    founds = findall_up(fun, rootpath; retfun = (container) -> length(container) == 1)
+    founds = findall_up(fun, rootpath; 
+        retfun = (path, container) -> length(container) == 1)
     return isempty(founds) ? nothing : founds |> first
 end
 find_up(name::String, rootpath) = 
-    find_up((path) -> basename(path) == name, rootpath);
+    find_up((path) -> basename(path) == name, rootpath; kwargs...);
