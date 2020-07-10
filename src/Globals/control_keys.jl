3fbd8a69ffcf83179e9333f7cc3e66b57293a669
josePereiro <jose.pereiro.lab@gmail.com>
2020-07-10 19:22:56 -0400

    Change control_keys, updated update_kill_status

--- src/Globals/control_keys.jl
+++ src/Globals/control_keys.jl
@@ -1,54 +1,18 @@
-"""
-    Store the information of the control keys.
-    Mainly type and description.
-"""
-# CONTROL_MANIFEST = Dict()
-# CONTROL_TYPE_KEY = "type"
-# CONTROL_DESC_KEY = "description"
+# TODO: add descriptions comment
+# ------------------- COMMONS -------------------
+OWNER_WORKER_NAME_KEY = "owner"
+VALUE_KEY = "value"
+UPDATE_DATE_KEY = "update_date"
+INFO_KEY = "info"
 
+# ------------------- ORIGIN_CONFIG -------------------
 EXE_ORDER_KEY = "exe_order"
-# CONTROL_MANIFEST[EXE_ORDER_KEY] = Dict(
-#     CONTROL_TYPE_KEY => Int,
-#     CONTROL_DESC_KEY => "Main control of tasks execution state, represent the current execution orider"
-# )
-
-EXE_ORDER_STATE_KEY = "$(EXE_ORDER_KEY)_state"
-# CONTROL_MANIFEST[EXE_ORDER_STATE_KEY] = Dict(
-#     CONTROL_TYPE_KEY => Bool,
-#     CONTROL_DESC_KEY => "The state of the $EXE_ORDER_KEY as resolved by the TaskManager from the control files state."
-# )
-
-EXE_ORDER_STATE_INFO_KEY = "$(EXE_ORDER_STATE_KEY)_info"
-# CONTROL_MANIFEST[EXE_ORDER_STATE_INFO_KEY] = Dict(
-#     CONTROL_TYPE_KEY => String,
-#     CONTROL_DESC_KEY => "Further info describing $EXE_ORDER_STATE_KEY"
-# )
-
-LAST_EXE_ORDER_KEY = "last_$(EXE_ORDER_KEY)"
-# CONTROL_MANIFEST[LAST_EXE_ORDER_KEY] = Dict(
-#     CONTROL_TYPE_KEY => Int,
-#     CONTROL_DESC_KEY => "Set the last execution order, "*
-#         "used to resolve if a execution order was already executed"
-# )
-
 KILL_SIGN_KEY = "kill_sign"
+
+# ------------------- LOCAL_STATUS -------------------
+LAST_EXE_ORDER_KEY = "last_$(EXE_ORDER_KEY)"
+KILL_STATE_KEY = "$(KILL_SIGN_KEY)_state"
+RUNNING_STATE_KEY = "running_state"
+
+# ------------------- VALUES -------------------
 KILL_SIGN = "kill"
-NOT_KILL_SIGN = "not_kill"
-
-# CONTROL_MANIFEST[KILL_SIGN_KEY] = Dict(
-#     CONTROL_TYPE_KEY => String,
-#     CONTROL_DESC_KEY => "Main control of tasks kill state, "*
-#         "if present the task will be killed or never run"
-# )
-
-KILL_ORDER_STATE_KEY = "$(KILL_SIGN_KEY)_state"
-# CONTROL_MANIFEST[KILL_ORDER_STATE_KEY] = Dict(
-#     CONTROL_TYPE_KEY => Bool,
-#     CONTROL_DESC_KEY => "The state of the $KILL_SIGN_KEY as resolved by the TaskManager from the control files state."
-# )
-
-KILL_ORDER_STATE_INFO_KEY = "$(KILL_ORDER_STATE_KEY)_info"
-# CONTROL_MANIFEST[KILL_ORDER_STATE_INFO_KEY] = Dict(
-#     CONTROL_TYPE_KEY => String,
-#     CONTROL_DESC_KEY => "Further info describing $KILL_ORDER_STATE_KEY"
-# )
---
+++ src/TaskManager/update_kill_status.jl
@@ -0,0 +1,50 @@
+"""
+    Use he current data available in ORIGIN_CONFIG to 
+    update the kill status of a given task in
+    LOCAL_STATUS.
+"""
+function update_kill_status(taskname)
+
+    # task LOCAL_STATUS
+    if !haskey(LOCAL_STATUS, taskname)
+        LOCAL_STATUS[taskname] = Dict()
+    end
+    
+    # kill state
+    if !haskey(LOCAL_STATUS[taskname], KILL_STATE_KEY)
+        LOCAL_STATUS[taskname][KILL_STATE_KEY] = Dict()
+    end
+    kill_state = LOCAL_STATUS[taskname][KILL_STATE_KEY]
+    
+    # ------------------- DEFAULT -------------------
+    kill_state[VALUE_KEY] = false
+    kill_state[INFO_KEY] = "Default"
+
+    # Update time
+    kill_state[UPDATE_DATE_KEY] = now()
+
+    # ------------------- CHECK ORIGIN -------------------
+    # no data, nothing to do
+    if !haskey(ORIGIN_CONFIG, taskname) 
+        kill_state[INFO_KEY] = "Task origin config missing"
+        return kill_state[VALUE_KEY] = false
+    end
+    origin_control = ORIGIN_CONFIG[taskname]
+
+    # ------------------- CHECK KILL SIGN -------------------
+    # no data, nothing to do
+    if !haskey(origin_control, KILL_SIGN_KEY) || 
+            !haskey(origin_control[KILL_SIGN_KEY], VALUE_KEY)
+        kill_state[INFO_KEY] = "Task origin $(KILL_SIGN_KEY) missing value"
+        return kill_state[VALUE_KEY] = false
+    end
+
+    # check kill sign
+    if origin_control[KILL_SIGN_KEY][VALUE_KEY] == KILL_SIGN
+        kill_state[INFO_KEY] = "$(KILL_SIGN_KEY) == $(KILL_SIGN). Die die die my darling!!!"
+        return kill_state[VALUE_KEY] = true
+    else
+        kill_state[INFO_KEY] = "$(KILL_SIGN_KEY) != $(KILL_SIGN)"
+        return kill_state[VALUE_KEY] = false
+    end
+end
