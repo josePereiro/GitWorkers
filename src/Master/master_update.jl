bc7870abdcfb976231a0a4acf2af422577eb3ec1
josePereiro <jose.pereiro.lab@gmail.com>
2020-07-14 10:26:41 -0400

    Improve master_update

--- src/Master/master_update.jl
+++ src/Master/master_update.jl
@@ -11,26 +11,25 @@ function master_update(path = pwd();
     # ------------------- MAKE CUSTOM TASK -------------------
     before_pull(worker)
 
-    # ------------------- SAVE REPO ORIGIND IN COPY -------------------
+    # ------------------- SAVE REPO ORIGINS IN COPY -------------------
     sync_taskdirs(FROM_REPO, ORIGIN_FOLDER_NAME, worker)
-
+    
     # ------------------- FORCE "PULL" -------------------
     # This force the local repo to be equal to the origin
     # This is a fundamental design desition. This way the 
     # worker code is more robust        
     !deb && git_pull(force = true, print = verbose)
+    read_config(worker)
+    read_status(worker)
+    
+    # ------------------- COPY BACK -------------------
+    sync_taskdirs(FROM_COPY, ORIGIN_FOLDER_NAME, worker)
 
     # ------------------- MAKE CUSTOM TASK -------------------
     before_push(worker)
 
-    # ------------------- COPY BACK -------------------
-    sync_taskdirs(FROM_COPY, ORIGIN_FOLDER_NAME, worker)
-
-    # writing
-    write_config(worker; create = true)
-
-    # TODO: introduce checks before pushing
     # ------------------- PUSH ORIGINS -------------------
+    write_config(worker; create = true)
     !deb && git_add_all(print = verbose)
     !deb && git_commit(commit_msg; print = verbose)
     !deb && git_push(force = true, print = verbose)
