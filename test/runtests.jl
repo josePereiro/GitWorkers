c067bf6e11940afb3e3ef934f161a15365e35036
josePereiro <jose.pereiro.lab@gmail.com>
2021-11-28 18:48:14 -0500

    Remove Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,4 @@ using Test
 
 @testset "GitWorkers.jl" begin
     
-    @testset "FileSystem" begin
-        include("FileSystem_tests.jl")
-    end
-    
-    @testset "TaskTest" begin
-        include("task_tests.jl")
-    end
-
 end
